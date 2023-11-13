95c9e8206c8030ecbd3ddc824a329fd85ea902ba
ebrahim <myemail@gmail.com>
2023-11-13 10:21:56 +0330

    done components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,7 @@ export default function DashboardLayout({
     return (
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
-        <nav></nav>
-   
+        <h1>Layout</h1>
         {children}
       </section>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 
 import React from 'react'
-import DashboardLayout from '@/Layout'
+import DashboardLayout from '@/components/Layout'
 export default function index() {
   return (
     <>
-    <DashboardLayout>
         <h1>Hello</h1>
-    </DashboardLayout>
     </>
   )
 }
